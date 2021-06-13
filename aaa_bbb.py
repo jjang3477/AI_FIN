cd49997a2dd24d994a6bb494d0f5a84e0444a989
jjango <jjang3477@naver.com>
2021-06-13 20:32:13 +0900

    Update comment

--- aaa_bbb.py
+++ aaa_bbb.py
@@ -7,14 +7,12 @@ start_time = datetime.date(21,01,01)
 end_time = datetime.datetime().now()
 
 ### date gathering
-
-
 ### 
-
-
 ###
-
 df = pd.read_csv("./List_KOSPI.csv")
 #print(df.head(5))
+
+## API 연동 
+
 
 
