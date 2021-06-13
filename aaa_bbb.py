5476ed6f5e71d684f00ff4c4bfe0344bbc3610c8
Jang Byungjun <jjang3477@naver.com>
2021-06-13 20:54:05 +0900

    Add files via upload
    test

---
+++ aaa_bbb.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pykrx import stock
+from datetime import datetime
+
+## define time 
+start_time = datetime.date(21,01,01)
+end_time = datetime.datetime().now()
+
+### date gathering
+### 
+###
+df = pd.read_csv("./List_KOSPI.csv")
+#print(df.head(5))
+
+## API 연동 
+
+
+
